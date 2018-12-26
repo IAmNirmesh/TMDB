e8f49fd1e7db1b85b1324c2e423fe5d995a597cd
dcodemonkey <nirmeshrahul@gmail.com>
2018-12-26 19:38:32 +0530

    Movies sorted by Most Popular & Most Rating

--- app/src/main/java/com/android/nirmesh/tmdb/SettingsActivity.java
+++ app/src/main/java/com/android/nirmesh/tmdb/SettingsActivity.java
@@ -1,4 +1,23 @@
 package com.android.nirmesh.tmdb;
 
-public class SettingsActivity {
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+import android.support.annotation.Nullable;
+
+public class SettingsActivity extends PreferenceActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new SettingsFragment()).commit();
+    }
+
+    public static class SettingsFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preferences);
+        }
+    }
 }
