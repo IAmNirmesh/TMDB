1b8d4a0639c441c471522c60e8a8227f1c629e1d
dcodemonkey <nirmeshrahul@gmail.com>
2018-12-30 22:55:44 +0530

    Enhanced Settings

--- app/src/main/java/com/android/nirmesh/tmdb/SettingsActivity.java
+++ app/src/main/java/com/android/nirmesh/tmdb/SettingsActivity.java
@@ -1,23 +1,14 @@
 package com.android.nirmesh.tmdb;
 
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
-import android.preference.PreferenceFragment;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 
-public class SettingsActivity extends PreferenceActivity {
+public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getFragmentManager().beginTransaction().replace(android.R.id.content, new SettingsFragment()).commit();
-    }
-
-    public static class SettingsFragment extends PreferenceFragment {
-        @Override
-        public void onCreate(@Nullable Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            addPreferencesFromResource(R.xml.preferences);
-        }
     }
 }
